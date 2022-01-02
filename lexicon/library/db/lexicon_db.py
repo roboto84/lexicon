f9d88f49fe277c9faf7c887d10dd2669ef01fbfe
roboto84 <7658704+roboto84@users.noreply.github.com>
2022-01-02 14:08:47 -0500

    fix(db): update how db sql paths are handled

--- lexicon/library/db/lexicon_db.py
+++ lexicon/library/db/lexicon_db.py
@@ -1,4 +1,5 @@
 import logging.config
+import os
 from willow_core.library.sqlite_db import SqlLiteDb
 from sqlite3 import Connection, Cursor, Error
 from typing import Any
@@ -11,6 +12,10 @@ class LexiconDb(SqlLiteDb):
         super().__init__(logging_object, db_location)
         self._check_db_schema()
 
+    @staticmethod
+    def add_file_path(relative_file_path: str) -> str:
+        return f'{os.path.dirname(__file__)}{relative_file_path}'
+
     def _check_db_schema(self) -> None:
         if self._check_db_state(['WORDS']):
             self._logger.info(f'DB schema looks good')
@@ -21,22 +26,22 @@ class LexiconDb(SqlLiteDb):
     def _create_db_schema(self) -> None:
         try:
             conn: Connection = self._db_connect()
-            with open('lexicon_bot/bin/db/sql/schema.sql') as f:
+            with open(self.add_file_path('/sql/schema.sql')) as f:
                 conn.executescript(f.read())
             self._logger.info(f'Initializing Air_DB schema')
             self._db_close(conn)
             self._logger.info(f'Database has been initialized')
         except Error as error:
-            self._logger.info(f'Error occurred initializing Air_DB', error)
+            self._logger.error(f'Error occurred initializing Air_DB: {str(error)}')
 
     def get_words(self):
         conn: Connection = self._db_connect()
         db_cursor: Cursor = conn.cursor()
-        return db_cursor.execute("""select * from WORDS order by time desc""").fetchall()
+        return db_cursor.execute("""select * from WORDS order by id desc""").fetchall()
 
     def insert_word(self, def_data: dict) -> None:
         word: str = def_data['word']
-        sql_path: str = 'lexicon_bot/bin/db/sql/insert_word.sql'
+        sql_path: str = self.add_file_path('/sql/insert_word.sql')
         conn: Connection = self._db_connect()
         db_cursor: Cursor = conn.cursor()
         table_list = db_cursor.execute("""SELECT word FROM WORDS WHERE word=?;""", [word]).fetchall()
@@ -50,10 +55,10 @@ class LexiconDb(SqlLiteDb):
                         (self._get_time(), word.lower(), word, def_data['date_first_used'], def_data['part_of_speech'],
                          def_data['word_break'], def_data['pronounce'], def_data['audio'],
                          str(def_data['etymology']), str(def_data['definitions']), str(def_data['example'])))
-            except IOError as e:
-                self._logger.info(f'IOError was thrown', e)
-            except Exception as e:
-                self._logger.exception(f'Exception was thrown', e)
+            except IOError as io_error:
+                self._logger.error(f'IOError was thrown: {str(io_error)}')
+            except Exception as exception:
+                self._logger.error(f'Exception was thrown: {str(exception)}')
         else:
             self._logger.info(f'"{word}" is already in the DB')
         self._db_close(conn)
--- lexicon/library/lexicon.py
+++ lexicon/library/lexicon.py
@@ -9,6 +9,59 @@ class Lexicon:
         self.lexicon_collect = LexiconCollect(webster_key, oxford_app_id, oxford_key)
         self.enchant_dictionary = enchant.Dict('en_US')
 
+    @staticmethod
+    def definition_is_acceptable(data: dict) -> bool:
+        webster_def_is_good = ('state' in data['merriam_webster'] and data['merriam_webster']['state'] != 'unavailable')
+        oxford_def_is_good = ('state' in data['oxford'] and data['oxford']['state'] != 'unavailable')
+        return webster_def_is_good or oxford_def_is_good
+
+    @staticmethod
+    def parse_dictionary_data(data: dict) -> dict:
+        date_first_used: str = ''
+        part_of_speech: str = ''
+        word_break: str = ''
+        pronounce: list[str] = []
+        stems: list[str] = []
+        audio: str = ''
+        etymology: str = ''
+        definitions: list[str] = []
+        example: str = ''
+
+        try:
+            if 'merriam_webster' in data:
+                mw = data['merriam_webster']
+                date_first_used = mw['date_first_used'] if ('date_first_used' in mw) else 'unk'
+                part_of_speech = mw['part_of_speech'] if ('part_of_speech' in mw) else 'unk'
+                word_break = mw['word_break'] if ('word_break' in mw) else 'unk'
+                stems = mw['stems'] if ('stems' in mw) else []
+                pronounce.append((mw['pronounce'] if ('pronounce' in mw) else 'unk'))
+                etymology = mw['etymology'] if ('etymology' in mw) else 'unk'
+                if 'definition' in mw and len(mw['definition']) > 0:
+                    definitions = mw['definition']
+
+            if 'oxford' in data:
+                ox = data['oxford']
+                audio = ox['audio'] if ('audio' in ox) else 'unk'
+                example = ox['example'] if ('example' in ox) else 'unk'
+                pronounce.append((ox['pronounce'] if ('pronounce' in ox) else 'unk'))
+                if 'definition' in ox and len(ox['definition']) > 0:
+                    definitions = definitions + ox['definition']
+
+            return {
+                'word': data["search_word"],
+                'stems': stems,
+                'date_first_used': date_first_used,
+                'part_of_speech': part_of_speech,
+                'word_break': word_break,
+                'pronounce': pronounce,
+                'audio': audio,
+                'etymology': etymology,
+                'definitions': definitions,
+                'example': example
+            }
+        except TypeError as type_error:
+            print(f'Received error (parse_dictionary_data): {str(type_error)}')
+
     def spell_checker(self, word: str) -> bool:
         return self.enchant_dictionary.check(word)
 
